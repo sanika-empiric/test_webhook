a089f15820a3abbdaf6a8ce51b6056c352a70389
sanika-empiric <sanika.empiric@gmail.com>
2023-10-13 16:54:16 +0530

    Modified test_file2.py

--- test_file2.py
+++ test_file2.py
@@ -4,6 +4,7 @@ from person2 import Person as Customer
 # Create objects of both classes
 employee = Employee("John", "E12345")
 customer = Customer("Alice", "C9876")
+customer = Customer("Sanika", "C98769")
 
 # Test methods from both classes
 print(employee.display())  # Output: Employee: John, ID: E12345
